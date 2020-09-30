764a03bba281516b8b0333fb458b53a2ea9694eb
nine-eleven <thedevilwearspradaoops@gmail.com>
2020-09-30 21:29:22 +0300

    fix: add function min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {return 0};
+  let minValue = array[0];
+  for (let arr of array) {
+    if (arr == undefined) {return 0};
+    if (arr < minValue) { minValue = arr };
+  }
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {return 0};
+  let maxValue = array[0];
+  for (let arr of array) {
+    if (arr == undefined) {return 0};
+    if (arr > maxValue) { maxValue = arr };
+  }
+  return maxValue;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {return 0};
+  let sum = 0;
+  for (let arr of array) {
+    if (arr == undefined) {return 0};
+    sum += arr;
+  }
+  return sum / array.length;
 }
